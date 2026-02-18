901af2ebe2c7bd93b927fba454ba2aeda1ab6e4d
karan <karansodhi703@gmail.com>
2026-02-18 17:54:52 +0530

    auth fixed

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,14 +1,46 @@
+// import express from "express";
+// import passport from "passport";
+
+// import { checkAuth, CreateUser, loginUser, resetPasswordRequest , resetPassword , logout} from "../controllers/AuthController.js";
+
+
+// const router = express.Router();
+// router.post("/login",passport.authenticate("local"), loginUser);
+// router.post("/signup",CreateUser );
+// router.get("/check",passport.authenticate("jwt"),checkAuth);
+// router.post('/reset-password-request', resetPasswordRequest)
+// router.post('/reset-password', resetPassword)
+// router.get("/logout",logout)
+// export default router;
 import express from "express";
 import passport from "passport";
-
-import { checkAuth, CreateUser, loginUser, resetPasswordRequest , resetPassword , logout} from "../controllers/AuthController.js";
-
+import {
+  checkAuth,
+  CreateUser,
+  loginUser,
+  resetPasswordRequest,
+  resetPassword,
+  logout
+} from "../controllers/AuthController.js";
 
 const router = express.Router();
-router.post("/login",passport.authenticate("local"), loginUser);
-router.post("/signup",CreateUser );
-router.get("/check",passport.authenticate("jwt"),checkAuth);
-router.post('/reset-password-request', resetPasswordRequest)
-router.post('/reset-password', resetPassword)
-router.get("/logout",logout)
+
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  loginUser
+);
+
+router.post("/signup", CreateUser);
+
+router.get(
+  "/check",
+  passport.authenticate("jwt", { session: false }),
+  checkAuth
+);
+
+router.post("/reset-password-request", resetPasswordRequest);
+router.post("/reset-password", resetPassword);
+router.get("/logout", logout);
+
 export default router;
